218d8c1f925f8da49b3d2751f509a27aa2e3ed7b
angoma850 <arian.angoma@tecsup.edu.pe>
2019-05-10 15:51:59 -0500

    Initial Commit

---
+++ practicaAngoma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practicaAngoma
+//
+//  Created by Arian Angoma on 10/05/19.
+//  Copyright © 2019 angoma.pe.edu.tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
